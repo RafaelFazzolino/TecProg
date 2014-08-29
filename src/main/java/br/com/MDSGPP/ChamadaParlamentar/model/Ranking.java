f7572e25670c5971d92e54a61672f000c43b3723
RafaelFazzolino <rafael_fazzolino@hotmail.com>
2014-08-29 14:24:52 -0300

    Adding new coments of "Ranking"

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/model/Ranking.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/model/Ranking.java
@@ -12,34 +12,51 @@ public class Ranking {
 	public Ranking() {
 		// TODO Auto-generated constructor stub
 	}
-	
+	/**
+	 * This method is to get the statistic of deturies.
+	 */
 	public ArrayList<Estatistica> getLista() {
 		return lista;
 	}
+	/**
+	 * This method is to change the variable lista.
+	 */
 	public void setLista(ArrayList<Estatistica> lista) {
 		this.lista = lista;
 	}
-
+	/**
+	 * This method is to get all deputies deleted.
+	 */
 	public ArrayList<Estatistica> getRemovidos() {
 		return removidos;
 	}
-
+	/**
+	 * This method is to change the variable removidos.
+	 */
 	public void setRemovidos(ArrayList<Estatistica> removidos) {
 		this.removidos = removidos;
 	}
-
+	/**
+	 * This method is to get the bests deputies.
+	 */
 	public ArrayList<Estatistica> getMelhores() {
 		return melhores;
 	}
-
+	/**
+	 * This method is to change the variable melhores.
+	 */
 	public void setMelhores(ArrayList<Estatistica> melhores) {
 		this.melhores = melhores;
 	}
-
+	/**
+	 * This method is to get the worst deputies.
+	 */
 	public ArrayList<Estatistica> getPiores() {
 		return piores;
 	}
-
+	/**
+	 * This method is to change the variable piores.
+	 */
 	public void setPiores(ArrayList<Estatistica> piores) {
 		this.piores = piores;
 	}
