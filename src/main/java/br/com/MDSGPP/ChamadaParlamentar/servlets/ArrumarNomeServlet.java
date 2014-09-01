71c294fdaa261f99ea3b424954e3e74c70588658
ThabataG <thabata.helen@gmail.com>
2014-09-01 12:14:17 -0300

    Comment the method of class ArrumarNomeServlet

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/servlets/ArrumarNomeServlet.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/servlets/ArrumarNomeServlet.java
@@ -11,6 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 public class ArrumarNomeServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * This is the only method of the servlet, its suppose to correct the name,
+	 * so it can be used to make search on the database.
+	 */
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = null;
