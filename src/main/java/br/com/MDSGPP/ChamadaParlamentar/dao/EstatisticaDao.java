a605668e144646d1ba338e6684b6e84a56e442d8
ThabataG <thabata.helen@gmail.com>
2014-09-01 11:45:08 -0300

    Comment the method of class EstatisticaDao

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/dao/EstatisticaDao.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/dao/EstatisticaDao.java
@@ -10,7 +10,16 @@ public class EstatisticaDao extends ConnectionFactory {
 	public EstatisticaDao() throws ClassNotFoundException, SQLException {
 		new ConnectionFactory().getConnection();
 	}
-
+	
+	/**
+	 * Method that adds all sessions assisted by Deputy in a list.
+	 * 
+	 * @param nome
+	 *            is the name of deputy.
+	 * @return an ArrayList with all sessions assisted by Deputy.
+	 * @throws SQLException
+	 *             if miss spelled SQL is entered.
+	 */
 
 	public ArrayList<String> getEstatisticaDeputados(String nome) throws SQLException {
 		String sql = "select * from sessao where deputadoPresente LIKE ?"; 
