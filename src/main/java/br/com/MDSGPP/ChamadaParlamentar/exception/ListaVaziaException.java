aa77dfeb5b4e8c5d2863fbbabc911acc90a8750e
ThabataG <thabata.helen@gmail.com>
2014-09-01 11:50:34 -0300

    Comment the method of class ListaVaziaException

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/ListaVaziaException.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/ListaVaziaException.java
@@ -7,6 +7,10 @@ package br.com.MDSGPP.ChamadaParlamentar.exception;
 public class ListaVaziaException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * This exception is to verify that the deputy has valid data or not.
+	 */
 
 	public ListaVaziaException() {
 		super();
