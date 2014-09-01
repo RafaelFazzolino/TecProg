a66ff1a4170497116d6620ae9becc65cd69b1d37
ThabataG <thabata.helen@gmail.com>
2014-09-01 11:37:37 -0300

    Comment the method of class SessoesEReunioesContro

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/control/SessoesEReunioesControl.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/control/SessoesEReunioesControl.java
@@ -7,6 +7,18 @@ import br.com.MDSGPP.ChamadaParlamentar.dao.SessoesEReunioesDao;
 import br.com.MDSGPP.ChamadaParlamentar.model.SessoesEReunioes;
 
 public final class SessoesEReunioesControl {
+	
+	/**
+	 * Method that bridges the gap between the Dao and Model about the sessions.
+	 * 
+	 * @param descricao
+	 *            is the parameter that used for search the session.
+	 * @return passar, is the object of the model SessoesEReunioes.
+	 * @throws ClassNotFoundException
+	 *             if the database is off.
+	 * @throws SQLException
+	 *             if miss spelled SQL is entered.
+	 */
 
 	public static SessoesEReunioes passarSessao(String descricao) 
 			throws ClassNotFoundException, SQLException {
@@ -20,6 +32,15 @@ public final class SessoesEReunioesControl {
 
 		return passar;		
 	}
+	
+	/**
+	 * Method that organizes how many deputies will be displayed per page.
+	 * 
+	 * @param pagina
+	 * @param deputadosPorPagina
+	 * @param deputadosPresentes
+	 * @return 'listaPassar' with all the deputies that are in the page.
+	 */
 
 	public static ArrayList<String> arrumarListaDeputados(int pagina, 
 			int deputadosPorPagina, ArrayList<String> deputadosPresentes) {
