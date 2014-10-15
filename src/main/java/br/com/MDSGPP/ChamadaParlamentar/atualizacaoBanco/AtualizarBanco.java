a9394fd2201df9c4e7697fa20f964965bb9e6ea9
Rafael Fazzolino <rafael_fazzolino@hotmail.com>
2014-10-15 14:14:18 -0300

    Adding coments of AtualizarBanco

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/atualizacaoBanco/AtualizarBanco.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/atualizacaoBanco/AtualizarBanco.java
@@ -21,7 +21,7 @@ import br.com.MDSGPP.ChamadaParlamentar.exception.ListaVaziaException;
 import br.com.MDSGPP.ChamadaParlamentar.model.Ranking;
 
 public class AtualizarBanco {
-	Timer timer;
+	Timer timer;/*Variable used to control the time of updating the database.*/
 	/**
 	 * Method to start the execution of tasks.
 	 * From this method will update the database with new information from the WebService.
@@ -35,15 +35,17 @@ public class AtualizarBanco {
 }
 	
 	private final static long delayDiario = 1000*60*60*24;
-	private final static int fONE_DAY = 0;
-	private final static int fFOUR_AM = 18;
-	private final static int fZERO_MINUTES = 17;
+	private final static int fONE_DAY = 0;/*Variable used to store the value 0, constant.*/
+	private final static int fFOUR_AM = 18;/*Variable used to store the value 18, constant.*/
+	private final static int fZERO_MINUTES = 17;/*Variable used to store the value 17, constant.*/
 
 	private static Date getAmanha6Am() {
 		System.out.println("entrou");
-	    Calendar tomorrow = new GregorianCalendar();
+	    Calendar tomorrow;/*Variable used to save the day that the update should be done.*/
+	    tomorrow = new GregorianCalendar();
 	    tomorrow.add(Calendar.DATE, fONE_DAY);
-	    Calendar result = new GregorianCalendar(
+	    Calendar result;/*Saves the time that the method should update the system.*/
+	    result = new GregorianCalendar(
 	    tomorrow.get(Calendar.YEAR),
 	    tomorrow.get(Calendar.MONTH),
 	    tomorrow.get(Calendar.DATE),
@@ -72,16 +74,21 @@ public class AtualizarBanco {
 				System.out.println("teste");
 				Dao.truncateTables();
 				
-				SessoesEReunioesDao sessionsDao = new SessoesEReunioesDao();
+				SessoesEReunioesDao sessionsDao;/*This variable controls the connection to the database, specific to the table sessions and meetings.*/
+				sessionsDao = new SessoesEReunioesDao();
 				
 				sessionsDao.adcionarDataNaTable(ConexaoComWsSessoesEReunioes.adcionarDataNaTable("20/11/2011", "440"));
 				sessionsDao.adcionarSessaoNaTable(ConexaoComWsSessoesEReunioes.adcionarSessaoNaTable("20/11/2011"));
-				RankingDao rankingDao = new RankingDao();
-				Ranking ranking;
+				
+				RankingDao rankingDao;/*This variable controls the connection to the database, specific to the ranking table.*/
+				rankingDao = new RankingDao();
+				
+				Ranking ranking;/*This variable is the ranking done.*/
 				ranking =  RankingControl.gerarRanking
 						(RankingControl.gerarListaEstatistica(new DeputadoDao().getDeputados()));
 		
 				rankingDao.adicionarRankingNaTable(ranking);
+				
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
