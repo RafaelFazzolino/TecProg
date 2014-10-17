ad7acf3d02fee928ca045a64ddb7dc78c7ac0f9c
joao-henrique <joaohenrique.pereira.almeida@gmail.com>
2014-10-16 23:21:48 -0300

    Change class

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/servlets/CatchListDeputy.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/servlets/CatchListDeputy.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import br.com.MDSGPP.ChamadaParlamentar.control.DeputadosControl;
 
-public class PegarListaDeputados extends HttpServlet {
+public class CatchListDeputy extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
