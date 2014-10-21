bfaef0840fb43a8667a4daf4b80c7699e39fd29a
Rafael Fazzolino <rafael_fazzolino@hotmail.com>
2014-10-21 14:21:57 -0200

    Update the code of DeputadoDao and DiaDao

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/dao/DeputadoDao.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/dao/DeputadoDao.java
@@ -9,54 +9,68 @@ import br.com.MDSGPP.ChamadaParlamentar.model.Deputados;
 
 public class DeputadoDao extends ConnectionFactory {
 
-	private static final int ONE = 1;/*Constant which takes the value 1.*/
-	private static final int TWO = 2;/*Constant which takes the value 2.*/
-	private static final int THREE = 3;/*Constant which takes the value 3.*/
-	private static final int FOUR = 4;/*Constant which takes the value 4.*/
-	private static final int FIVE = 5;/*Constant which takes the value 5.*/
-	private static final int SIX = 6;/*Constant which takes the value 6.*/
-	private static final int SEVEN = 7;/*Constant which takes the value 7.*/
-	private static final int EIGHT = 8;/*Constant which takes the value 8.*/
-	private static final int NINE = 9;/*Constant which takes the value 9.*/
-	private static final int TEN = 10;/*Constant which takes the value 10.*/
-	private static final int ELEVEN = 11;/*Constant which takes the value 11.*/
-	private static final int TWELVE = 12;/*Constant which takes the value 12.*/
-		
+	private static final int ONE = 1;/* Constant which takes the value 1. */
+	private static final int TWO = 2;/* Constant which takes the value 2. */
+	private static final int THREE = 3;/* Constant which takes the value 3. */
+	private static final int FOUR = 4;/* Constant which takes the value 4. */
+	private static final int FIVE = 5;/* Constant which takes the value 5. */
+	private static final int SIX = 6;/* Constant which takes the value 6. */
+	private static final int SEVEN = 7;/* Constant which takes the value 7. */
+	private static final int EIGHT = 8;/* Constant which takes the value 8. */
+	private static final int NINE = 9;/* Constant which takes the value 9. */
+	private static final int TEN = 10;/* Constant which takes the value 10. */
+	private static final int ELEVEN = 11;/* Constant which takes the value 11. */
+	private static final int TWELVE = 12;/* Constant which takes the value 12. */
+
 	/**
 	 * This method is to create the instance of ConnectionFactory.
-	 * @throws ClassNotFoundException case the class is not found.
-	 * @throws SQLException case occurs an error with database.
+	 * 
+	 * @throws ClassNotFoundException
+	 *             case the class is not found.
+	 * @throws SQLException
+	 *             case occurs an error with database.
 	 */
 	public DeputadoDao() throws ClassNotFoundException, SQLException {
 		new ConnectionFactory().getConnection();
 	}
-	
+
 	/**
 	 * This method add a deputy in the list Deputies.
-	 * @param deputados is an ArrayList contains all deputies.
-	 * @throws SQLException case the dataBase is off.
+	 * 
+	 * @param deputados
+	 *            is an ArrayList contains all deputies.
+	 * @throws SQLException
+	 *             case the dataBase is off.
 	 */
-	
-	public void adicionaDeputado(ArrayList<Deputados> deputies) throws SQLException {
-		String sql;/*Variable that received the code to add deputies on database.*/
+
+	public void adicionaDeputado(ArrayList<Deputados> deputies)
+			throws SQLException {
+		String sql;/*
+					 * Variable that received the code to add deputies on
+					 * database.
+					 */
 		sql = "insert into deputado(idParlamentar, matricula, ideCadastro, "
 				+ "nomeCivil, nomeDeTratamento, sexo, uf, partido"
-				+ ", numeroDoGabinete, anexo, telefone, email)" +
-				"values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				+ ", numeroDoGabinete, anexo, telefone, email)"
+				+ "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-		PreparedStatement stmt;/*Variable that create the connection with database.*/
+		PreparedStatement stmt;/*
+								 * Variable that create the connection with
+								 * database.
+								 */
 		stmt = getConexao().prepareStatement(sql);
-		
-		int sizeDeputies;/*Variable that contains the size of deputies.*/
+
+		int sizeDeputies;/* Variable that contains the size of deputies. */
 		sizeDeputies = deputies.size();
-		
-		for( int i = 0; i < sizeDeputies; i++ ) {
+
+		for (int i = 0; i < sizeDeputies; i++) {
 
 			stmt.setInt(ONE, deputies.get(i).getIdDoParlamentar());
 			stmt.setInt(TWO, deputies.get(i).getMatricula());
 			stmt.setInt(THREE, deputies.get(i).getIdeCadastro());
 			stmt.setString(FOUR, deputies.get(i).getNomeCivilDoParlamentar());
-			stmt.setString(FIVE, deputies.get(i).getNomeDeTratamentoDoParlamentar());
+			stmt.setString(FIVE, deputies.get(i)
+					.getNomeDeTratamentoDoParlamentar());
 			stmt.setString(SIX, deputies.get(i).getSexo());
 			stmt.setString(SEVEN, deputies.get(i).getUf());
 			stmt.setString(EIGHT, deputies.get(i).getPartido());
@@ -68,36 +82,47 @@ public class DeputadoDao extends ConnectionFactory {
 			stmt.execute();
 
 		}
-		stmt.close();	
+		stmt.close();
 	}
-	
+
 	/**
 	 * This method is used in the auto-complete in the search of a name.
+	 * 
 	 * @return list contains the name of all deputies.
-	 * @throws SQLException case the dataBase is off.
+	 * @throws SQLException
+	 *             case the dataBase is off.
 	 */
-	
-	public ArrayList<String> getNomesDeputados() throws SQLException {
-		String sql;/*Variable that contains the code to database.*/
-		sql = "Select * from deputado"; 
 
-		ArrayList<String> list;/*This is an Array that contains all deputies.*/
+	public ArrayList<String> getNomesDeputados() throws SQLException {
+		String sql;/* Variable that contains the code to database. */
+		sql = "Select * from deputado";
+
+		ArrayList<String> list;/* This is an Array that contains all deputies. */
 		list = new ArrayList<String>();
 
-		PreparedStatement stmt;/*Variable that create the connection with database.*/
+		PreparedStatement stmt;/*
+								 * Variable that create the connection with
+								 * database.
+								 */
 		stmt = ConnectionFactory.getConexao().prepareStatement(sql);
-		
-		ResultSet rs;/*Variable that received the connection.*/
+
+		ResultSet rs;/* Variable that received the connection. */
 		rs = stmt.executeQuery();
 
-		while( rs.next() ) {
-			
-			String civilName;/*Variable that contains the original name of deputy.*/
+		while (rs.next()) {
+
+			String civilName;/*
+							 * Variable that contains the original name of
+							 * deputy.
+							 */
 			civilName = rs.getString("nomeCivil");
-			
-			String tratamentName;/*Variable that contains the Treatment Name of deputy.*/
+
+			String tratamentName;/*
+								 * Variable that contains the Treatment Name of
+								 * deputy.
+								 */
 			tratamentName = rs.getString("nomeDeTratamento");
-			
+
 			list.add(civilName);
 			list.add(tratamentName);
 		}
@@ -105,78 +130,87 @@ public class DeputadoDao extends ConnectionFactory {
 		rs.close();
 		stmt.close();
 
-		
 		return list;
 
 	}
+
 	/**
 	 * This method is to get the enrolls deputy.
+	 * 
 	 * @return list what is the Array contains all enrolls of deputies.
-	 * @throws SQLException is case occurs an error with dataBase.
+	 * @throws SQLException
+	 *             is case occurs an error with dataBase.
 	 */
 	public ArrayList<Integer> getMatriculaDeputados() throws SQLException {
-		
+
 		/**
 		 * Creates the SQL command search how to look for an specific line.
 		 */
-		
-		String sql;/*Variable that received the code of database.*/
+
+		String sql;/* Variable that received the code of database. */
 		sql = "Select * from deputado";
 
-		ArrayList<Integer> list;/*This is an array that contains all deputies.*/
+		ArrayList<Integer> list;/* This is an array that contains all deputies. */
 		list = new ArrayList<Integer>();
 
 		/**
-		 * Creates the prepared statement that is what is going to connect with the DB.
+		 * Creates the prepared statement that is what is going to connect with
+		 * the DB.
 		 */
-		
-		PreparedStatement stmt= ConnectionFactory.getConexao().prepareStatement(sql);
-		
+
+		PreparedStatement stmt = ConnectionFactory.getConexao()
+				.prepareStatement(sql);
+
 		/**
 		 * Execute the STMT to search the data.
 		 */
-		
-		ResultSet rs;/*Variable that received the connection.*/
+
+		ResultSet rs;/* Variable that received the connection. */
 		rs = stmt.executeQuery();
 
-		while( rs.next() ) {
+		while (rs.next()) {
 			list.add(rs.getInt("matricula"));
 		}
-
 
 		rs.close();
 		return list;
 	}
-	
+
 	/**
 	 * This method is to get all deputies in dataBase.
+	 * 
 	 * @return lista what is an array contains all deputies.
-	 * @throws SQLException is case occurs an error with dataBase.
+	 * @throws SQLException
+	 *             is case occurs an error with dataBase.
 	 */
 
 	public ArrayList<Deputados> getDeputados() throws SQLException {
-		String sql;/*Variable that received the code of database.*/
+		String sql;/* Variable that received the code of database. */
 		sql = "Select * from deputado";
-		
-		ArrayList<Deputados> list;/*This is an array that contains all deputies.*/
+
+		ArrayList<Deputados> list;/* This is an array that contains all deputies. */
 		list = new ArrayList<Deputados>();
 
-		PreparedStatement stmt;/*Variable that create the connection.*/
+		PreparedStatement stmt;/* Variable that create the connection. */
 		stmt = ConnectionFactory.getConexao().prepareStatement(sql);
 
-		ResultSet rs;/*Variable that received the connection.*/
+		ResultSet rs;/* Variable that received the connection. */
 		rs = stmt.executeQuery();
 
-		while( rs.next() ) {
-			Deputados deputy;/*Variable that received all features of the deputy.*/
+		while (rs.next()) {
+			Deputados deputy;/*
+							 * Variable that received all features of the
+							 * deputy.
+							 */
 			deputy = new Deputados();
 
 			deputy.setIdDoParlamentar(rs.getInt("idParlamentar"));
 			deputy.setMatricula(rs.getInt("matricula"));
 			deputy.setIdeCadastro(rs.getInt("ideCadastro"));
 			deputy.setNomeCivilDoParlamentar(rs.getString("nomeCivil"));
-			deputy.setNomeDeTratamentoDoParlamentar(rs.getString("nomeDeTratamento"));
-			deputy.setSexo(rs.getString("sexo"));		
+			deputy.setNomeDeTratamentoDoParlamentar(rs
+					.getString("nomeDeTratamento"));
+			deputy.setSexo(rs.getString("sexo"));
 			deputy.setUf(rs.getString("uf"));
 			deputy.setPartido(rs.getString("partido"));
 			deputy.setNumeroDoGabinete(rs.getString("numeroDoGabinete"));
--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/dao/DiaDao.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/dao/DiaDao.java
@@ -11,37 +11,51 @@ import br.com.MDSGPP.ChamadaParlamentar.model.SessoesEReunioes;
 
 public class DiaDao extends ConnectionFactory {
 
-	private static final String DATAS = "datas";/*This is a constant that contains the string "datas".*/
+	private static final String DATAS = "datas";/*
+												 * This is a constant that
+												 * contains the string "datas".
+												 */
 
 	/**
 	 * This method is to create the connection.
-	 * @throws ClassNotFoundException case the class is not found.
-	 * @throws SQLException case the database is off.
+	 * 
+	 * @throws ClassNotFoundException
+	 *             case the class is not found.
+	 * @throws SQLException
+	 *             case the database is off.
 	 */
 	public DiaDao() throws ClassNotFoundException, SQLException {
 		new ConnectionFactory().getConnection();
 	}
-	
+
 	/**
 	 * This method search all the descriptions based on a day.
+	 * 
 	 * @return list contains all days.
-	 * @throws SQLException case the dataBase is off.
-	 * @throws DataFormatoErradoException case the date is wrong.
+	 * @throws SQLException
+	 *             case the dataBase is off.
+	 * @throws DataFormatoErradoException
+	 *             case the date is wrong.
 	 */
 
-	public ArrayList<Dia> buscarTodasDescricoes() throws SQLException, DataFormatoErradoException {
-		ArrayList<Dia> list;/*Variable that contains all days.*/
+	public ArrayList<Dia> buscarTodasDescricoes() throws SQLException,
+			DataFormatoErradoException {
+		ArrayList<Dia> list;/* Variable that contains all days. */
 		list = new ArrayList<Dia>();
-		
-		String sql;/*Variable that contains the code of database.*/
+
+		String sql;/* Variable that contains the code of database. */
 		sql = "Select * from datas";
 
-		PreparedStatement stmt= ConnectionFactory.getConexao().prepareStatement(sql);
-		ResultSet rs = stmt.executeQuery();
+		PreparedStatement stmt;/* Variable that create the connection. */
+		stmt = ConnectionFactory.getConexao().prepareStatement(sql);
+
+		ResultSet rs;/* Variable that received the connection. */
+		rs = stmt.executeQuery();
 
 		list = criarDias(rs);
 
-		ResultSet rs2 = stmt.executeQuery();
+		ResultSet rs2;/* Variable that received the connection. */
+		rs2 = stmt.executeQuery();
 
 		list = popularListaDia(rs2, list);
 
@@ -51,26 +65,39 @@ public class DiaDao extends ConnectionFactory {
 
 	/**
 	 * This method populates the DB based on a day.
+	 * 
 	 * @param rs
 	 * @param lista
 	 * @return list contains all days.
-	 * @throws SQLException case the dataBase is off.
+	 * @throws SQLException
+	 *             case the dataBase is off.
 	 */
-	public static ArrayList<Dia> popularListaDia(ResultSet rs, 
+	public static ArrayList<Dia> popularListaDia(ResultSet rs,
 			ArrayList<Dia> list) throws SQLException {
-		int cont = 0;
+		int cont;/* Variable to use on while. */
+		cont = 0;
 
-		while( rs.next() ) {
-			String descricao = rs.getString("sessao");
-			String descAux = descricao;
+		while (rs.next()) {
+			String descricao;/* Variable that received the description. */
+			descricao = rs.getString("sessao");
+
+			String descAux;/* Temporary variable that received the description. */
+			descAux = descricao;
 			descricao = descricao.split(" -")[0];
 
-			boolean teste = list.get(cont).getData().equalsIgnoreCase(rs.getString(DATAS));
+			boolean teste;/* Variable for test the table of database. */
+			teste = list.get(cont).getData()
+					.equalsIgnoreCase(rs.getString(DATAS));
 
-			if( !teste ) {
+			if (!teste) {
 				cont++;
 			}
-			SessoesEReunioes passar = new SessoesEReunioes();
+
+			SessoesEReunioes passar;/*
+									 * Variable that received all features of
+									 * the sessions.
+									 */
+			passar = new SessoesEReunioes();
 			passar.setDescricao(descricao);
 			passar.setFullDescription(descAux);
 			list.get(cont).getListaSessoes().add(passar);
@@ -82,37 +109,44 @@ public class DiaDao extends ConnectionFactory {
 
 	/**
 	 * This method create the Dia in the DB.
+	 * 
 	 * @param rs
-	 * @return lista contains the new days.
-	 * @throws SQLException case the dataBase is off.
-	 * @throws DataFormatoErradoException case the date is wrong.
+	 * @return list contains the new days.
+	 * @throws SQLException
+	 *             case the dataBase is off.
+	 * @throws DataFormatoErradoException
+	 *             case the date is wrong.
 	 */
-	
-	public static ArrayList<Dia> criarDias(ResultSet rs) 
-			throws SQLException, DataFormatoErradoException {
-		ArrayList<Dia> lista = new ArrayList<Dia>();
 
-		int controle = 0;
+	public static ArrayList<Dia> criarDias(ResultSet rs) throws SQLException,
+			DataFormatoErradoException {
+		ArrayList<Dia> list;/* Is an array that received the days. */
+		list = new ArrayList<Dia>();
 
-		while( rs.next() ) {
-			if( lista.size() == 0 ) {
-				Dia dia = new Dia();
+		int controle;/* This variable is to control the while. */
+		controle = 0;
+
+		while (rs.next()) {
+			if (list.size() == 0) {
+				Dia dia;/* Variable that contains the day. */
+				dia = new Dia();
 				dia.setData(rs.getString(DATAS));
-				lista.add(dia);
-			}
-			else {
-				boolean teste = lista.get(controle).getData().equals(rs.getString(DATAS));
-				if( !teste ) {
-					Dia diaAux = new Dia();
+				list.add(dia);
+			} else {
+				boolean teste = list.get(controle).getData()
+						.equals(rs.getString(DATAS));
+				if (!teste) {
+					Dia diaAux;/* Temporary variable that contains the day. */
+					diaAux = new Dia();
 					diaAux.setData(rs.getString(DATAS));
-					lista.add(diaAux);
+					list.add(diaAux);
 					controle++;
 				}
 			}
 		}
 
 		rs.close();
-		return lista;
+		return list;
 	}
 
 }
