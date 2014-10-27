4a6504e6b997b41615fc3066efc5586ff684287b
Rafael Fazzolino <rafael_fazzolino@hotmail.com>
2014-10-27 10:58:32 -0200

    Organizing the tests

--- src/test/java/br/com/MDSGPP/ChamadaParlamentar/util/teste/TesteLimparLista.java
+++ src/test/java/br/com/MDSGPP/ChamadaParlamentar/util/teste/TesteLimparLista.java
@@ -12,53 +12,48 @@ import br.com.MDSGPP.ChamadaParlamentar.model.Statistic;
 import br.com.MDSGPP.ChamadaParlamentar.util.LimparLista;
 
 public class TesteLimparLista {
-	
+
 	LimparLista limpar;
-	
+
 	@Before
 	public void setUp() {
 		limpar = new LimparLista();
 	}
-	
+
 	@Test
 	public void testLimparListaConstrutor() {
 		assertNotNull(limpar);
 	}
-	
+
 	@Test
 	public void testLimparLista() {
 		ArrayList<Statistic> lista = new ArrayList<Statistic>();
-		for(int i = 0; i<10; i++) {
+		for (int i = 0; i < 10; i++) {
 			Statistic estatistica = Mockito.spy(new Statistic());
 			Mockito.when(estatistica.getNumberSession()).thenReturn("50");
 			lista.add(estatistica);
 		}
-		
-		for(int i = 0; i<10; i++) {
+
+		for (int i = 0; i < 10; i++) {
 			Statistic estatistica = Mockito.spy(new Statistic());
 			lista.add(estatistica);
 		}
-		
-		ArrayList<ArrayList<Statistic>> listaRecebida = LimparLista.limparLista(lista);
-		
+
+		ArrayList<ArrayList<Statistic>> listaRecebida = LimparLista
+				.limparLista(lista);
+
 		assertNotNull(listaRecebida);
 		ArrayList<Statistic> bons = listaRecebida.get(0);
 		ArrayList<Statistic> ruins = listaRecebida.get(1);
-		
+
 		assertNotNull(bons);
 		assertNotNull(ruins);
-		
-		for(int i = 0; i<bons.size(); i++) {
+
+		for (int i = 0; i < bons.size(); i++) {
 			assertTrue(bons.get(i).getNumberSession().equalsIgnoreCase("50"));
 			assertTrue(ruins.get(i).getNumberSession() == null);
 		}
-		
+
 	}
 
 }
-
-
-
-
-
-
