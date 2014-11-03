2055fa5691676340b53abfddf71d390de2b52d7a
Rafael Fazzolino <rafael_fazzolino@hotmail.com>
2014-11-03 00:33:37 -0200

    Create atomic function

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/classesDeConexao/ConnectionWithWsSessions.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/classesDeConexao/ConnectionWithWsSessions.java
@@ -7,12 +7,12 @@
  * This class is responsible to establish a connection to the webService Deputados 
  * from the Deputy chamber, and return data.
  */
-
 package br.com.MDSGPP.ChamadaParlamentar.classesDeConexao;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.security.Security;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,8 +26,11 @@ import org.w3c.dom.NodeList;
 
 import br.com.MDSGPP.ChamadaParlamentar.dao.DeputiesDao;
 import br.gov.camara.www.SitCamaraWS.SessoesReunioes.ListPresencesCongressmanResponseListPresencesCongressmanResult;
+import br.gov.camara.www.SitCamaraWS.SessoesReunioes.ListarPresencasParlamentarResponse;
 import br.gov.camara.www.SitCamaraWS.SessoesReunioes.SessoesReunioesLocator;
 import br.gov.camara.www.SitCamaraWS.SessoesReunioes.SessoesReunioesSoapStub;
+
+import com.sun.mail.imap.protocol.Status;
 
 public class ConnectionWithWsSessions {
 
@@ -45,15 +48,13 @@ public class ConnectionWithWsSessions {
 	 * @throws ServiceException
 	 *             if there is a problem with the connection.
 	 */
-
-	public static SessoesReunioesSoapStub obtainConnection()
+	public static SessoesReunioesSoapStub obterConexao()
 			throws MalformedURLException, ServiceException {
-		URL url;/* Variable used to receive the site of the webservice. */
+		URL url;
 		url = new URL(
 				"http://www.camara.gov.br/SitCamaraWS/SessoesReunioes.asmx");
 
-		SessoesReunioesSoapStub service;/* create the connection of sessions. */
-		service = (SessoesReunioesSoapStub) new SessoesReunioesLocator()
+		SessoesReunioesSoapStub service = (SessoesReunioesSoapStub) new SessoesReunioesLocator()
 				.getSessoesReunioesSoap(url);
 
 		return service;
@@ -72,21 +73,13 @@ public class ConnectionWithWsSessions {
 	 *            is the ID of the Deputy.
 	 * @return the sessions.
 	 */
-
 	public static ListPresencesCongressmanResponseListPresencesCongressmanResult receberElementPresenca(
 			SessoesReunioesSoapStub service, String inicio, String fim,
 			String matricula) {
 
+		// conexao criada, agora chamaremos a classe do ws
 		try {
-			ListPresencesCongressmanResponseListPresencesCongressmanResult sessoes;/*
-																						 * Variable
-																						 * that
-																						 * contains
-																						 * the
-																						 * sessions
-																						 * .
-																						 */
-			sessoes = service
+			ListPresencesCongressmanResponseListPresencesCongressmanResult sessoes = service
 					.listarPresencasParlamentar(inicio, fim, matricula);
 
 			return sessoes;
@@ -113,131 +106,18 @@ public class ConnectionWithWsSessions {
 	 * @throws ServiceException
 	 *             if there is a problem with the connection.
 	 */
-
-	public static ArrayList<String> addSessionInTable(String data)
+	public static ArrayList<String> adcionarSessaoNaTable(String data)
 			throws SQLException, ClassNotFoundException, MalformedURLException,
 			ServiceException {
-		ArrayList<String> foi;
-		foi = new ArrayList<String>();
-		ArrayList<Integer> list;
-		list = new ArrayList<Integer>();
+		ArrayList<String> foi = new ArrayList<String>();
+		ArrayList<Integer> lista = new ArrayList<Integer>();
 
-		DeputiesDao conectionDeputy;/*
-									 * Variable that create the connection with
-									 * deputies.
-									 */
-		conectionDeputy = new DeputiesDao();
+		DeputiesDao conexaoDeputado = new DeputiesDao();
 
-		list = conectionDeputy.getMatriculaDeputados();
+		lista = conexaoDeputado.getMatriculaDeputados();
 
-		int sizeList;/* Variable that contains the size of the list. */
-		sizeList = list.size();
+		foi = getDateFromWs(lista, foi, data);
 
-		for (int i = 0; i < sizeList; i++) {
-
-			double percentage; /*
-								 * Variable that will contains the percentage of
-								 * assisted sessions.
-								 */
-			percentage = (((double) (i) / (double) sizeList) * 100.0);
-
-			System.out.println(i + "- " + percentage + "%");
-			Calendar today; /* Variable that will contains the date of today. */
-			today = new GregorianCalendar();
-
-			SimpleDateFormat df;/*
-								 * Variable that contains the simple format of
-								 * date.
-								 */
-			df = new SimpleDateFormat();
-			df.applyPattern("dd/MM/yyyy");
-
-			ListPresencesCongressmanResponseListPresencesCongressmanResult sessao;/*
-																					 * Variable
-																					 * that
-																					 * contains
-																					 * the
-																					 * sessions
-																					 * .
-																					 */
-			try {
-				sessao = ConnectionWithWsSessions.receberElementPresenca(
-						ConnectionWithWsSessions.obtainConnection(), data,
-						df.format(today.getTime()),
-						Integer.toString(list.get(i)));
-
-				NodeList days;/* Variable that contains the days. */
-				days = sessao.get_any()[0].getElementsByTagName("dia");
-
-				int sizeDays;/* Variable that contains the size of the days. */
-				sizeDays = days.getLength();
-
-				for (int j = 0; j < sizeDays; j++) {
-
-					MessageElement diasTemp;/*
-											 * Variable temporary that contains
-											 * the days.
-											 */
-					diasTemp = (MessageElement) days.item(j);
-
-					NodeList descricaoTemp;/*
-											 * Variable temporary that contains
-											 * the description of the session.
-											 */
-					descricaoTemp = diasTemp.getElementsByTagName("descricao");
-
-					NodeList presencaTemp;/*
-										 * Variable temporary that contains the
-										 * presence.
-										 */
-					presencaTemp = diasTemp.getElementsByTagName("frequencia");
-
-					int descriptionSize;/*
-										 * Variable that contains the size of
-										 * description.
-										 */
-					descriptionSize = descricaoTemp.getLength();
-
-					for (int k = 0; k < descriptionSize; k++) {
-
-						MessageElement descricaoText;/*
-													 * Variable that contains
-													 * the text of description.
-													 */
-						descricaoText = (MessageElement) descricaoTemp.item(k);
-
-						NodeList nomeTemp;/*
-										 * Variable temporary that contains the
-										 * name of the session.
-										 */
-						nomeTemp = sessao.get_any()[0]
-								.getElementsByTagName("nomeParlamentar");
-
-						MessageElement nomeText;/*
-												 * Variable that contains the
-												 * text of the name.
-												 */
-						nomeText = (MessageElement) nomeTemp.item(0);
-
-						MessageElement presencaText;/*
-													 * Variable that contains
-													 * the text of the presence.
-													 */
-						presencaText = (MessageElement) presencaTemp.item(k);
-
-						if (presencaText.getFirstChild().getNodeValue()
-								.equals("Presença")) {
-
-							foi.add(descricaoText.getFirstChild()
-									.getNodeValue());
-							foi.add(nomeText.getFirstChild().getNodeValue());
-						}
-					}
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
 		return foi;
 	}
 
@@ -248,7 +128,7 @@ public class ConnectionWithWsSessions {
 	 *            , is the date of the session.
 	 * @param matricula
 	 *            , is the ID of the Deputy.
-	 * @return pass, to know if the goal has been completed.
+	 * @return passar, to know if the goal has been completed.
 	 * @throws SQLException
 	 *             if miss spelled SQL is entered.
 	 * @throws MalformedURLException
@@ -256,83 +136,117 @@ public class ConnectionWithWsSessions {
 	 * @throws ServiceException
 	 *             if there is a problem with the connection.
 	 */
-
-	public static ArrayList<String> addDateInTable(String data,
+	public static ArrayList<String> adcionarDataNaTable(String data,
 			String matricula) throws SQLException, MalformedURLException,
 			ServiceException {
-		ArrayList<String> pass = new ArrayList<String>();
+		ArrayList<String> passar = new ArrayList<String>();
 
-		ListPresencesCongressmanResponseListPresencesCongressmanResult sessao;/*
-																				 * Variable
-																				 * that
-																				 * contains
-																				 * the
-																				 * sessions
-																				 * .
-																				 */
+		ListPresencesCongressmanResponseListPresencesCongressmanResult sessao;
 
-		Calendar today; /* Variable that will contains the date of today. */
-		today = new GregorianCalendar();
+		Calendar hoje = new GregorianCalendar();
 
-		SimpleDateFormat df;/*
-							 * Variable that contains the simple format of the
-							 * date.
-							 */
-		df = new SimpleDateFormat();
+		SimpleDateFormat df = new SimpleDateFormat();
 		df.applyPattern("dd/MM/yyyy");
 
 		sessao = ConnectionWithWsSessions.receberElementPresenca(
-				ConnectionWithWsSessions.obtainConnection(), data,
-				df.format(today.getTime()), matricula);
+				ConnectionWithWsSessions.obterConexao(), data,
+				df.format(hoje.getTime()), matricula);
 
-		NodeList days;/* Variable that contains the days. */
-		days = sessao.get_any()[0].getElementsByTagName("dia");
+		NodeList dias = sessao.get_any()[0].getElementsByTagName("dia");
 
-		int sizeDays;
-		sizeDays = days.getLength();
+		for (int i = 0; i < dias.getLength(); i++) {
+			MessageElement diaTemp = (MessageElement) dias.item(i);
 
-		for (int i = 0; i < sizeDays; i++) {
+			NodeList dataTemp = diaTemp.getElementsByTagName("data");
+			NodeList descricaoTemp = diaTemp.getElementsByTagName("descricao");
 
-			MessageElement diaTemp;/*
-									 * Variable temporary that contains the
-									 * days.
-									 */
-			diaTemp = (MessageElement) days.item(i);
+			MessageElement dataText = (MessageElement) dataTemp.item(0);
 
-			NodeList dataTemp;/* Variable temporary that contains the date. */
-			dataTemp = diaTemp.getElementsByTagName("data");
-
-			NodeList descricaoTemp;/*
-									 * Variable temporary that contains the
-									 * description of the session.
-									 */
-			descricaoTemp = diaTemp.getElementsByTagName("descricao");
-
-			MessageElement dataText;/*
-									 * Variable that contains the text of the
-									 * date.
-									 */
-			dataText = (MessageElement) dataTemp.item(0);
-
-			int descriptionSize;/*
-								 * Variable that contains the size of
-								 * description.
-								 */
-			descriptionSize = descricaoTemp.getLength();
-
-			for (int j = 0; j < descriptionSize; j++) {
-
-				MessageElement descricaoText;/*
-											 * Variable that contains the text
-											 * of description.
-											 */
-				descricaoText = (MessageElement) descricaoTemp.item(j);
+			for (int j = 0; j < descricaoTemp.getLength(); j++) {
+				MessageElement descricaoText = (MessageElement) descricaoTemp
+						.item(j);
 
 				System.out.println(dataText.getFirstChild().getNodeValue());
-				pass.add(dataText.getFirstChild().getNodeValue());
-				pass.add(descricaoText.getFirstChild().getNodeValue());
+				passar.add(dataText.getFirstChild().getNodeValue());
+				passar.add(descricaoText.getFirstChild().getNodeValue());
 			}
 		}
-		return pass;
+		return passar;
 	}
+
+	private static ArrayList<String> getActualTextForSession(
+			ArrayList<String> foi,
+			ListPresencesCongressmanResponseListPresencesCongressmanResult sessao,
+			NodeList descricaoTemp, NodeList presencaTemp, int k) {
+		MessageElement descricaoText = (MessageElement) descricaoTemp.item(k);
+		NodeList nomeTemp = sessao.get_any()[0]
+				.getElementsByTagName("nomeParlamentar");
+		MessageElement nomeText = (MessageElement) nomeTemp.item(0);
+		MessageElement presencaText = (MessageElement) presencaTemp.item(k);
+
+		if (presencaText.getFirstChild().getNodeValue().equals("Presença")) {
+
+			foi.add(descricaoText.getFirstChild().getNodeValue());
+			foi.add(nomeText.getFirstChild().getNodeValue());
+		}
+		return foi;
+	}
+
+	private static void showProgress(final int placeOnArray, final int total) {
+		double porcentagem = (((double) (placeOnArray) / (double) total) * 100.0);
+
+		System.out.println(placeOnArray + "- " + porcentagem + "%");
+	}
+
+	private static ArrayList<String> getNoListsOfDias(
+			ArrayList<String> foi,
+			NodeList dias,
+			ListPresencesCongressmanResponseListPresencesCongressmanResult sessao) {
+		for (int j = 0; j < dias.getLength(); j++) {
+
+			MessageElement diasTemp = (MessageElement) dias.item(j);
+			NodeList descricaoTemp = diasTemp.getElementsByTagName("descricao");
+			NodeList presencaTemp = diasTemp.getElementsByTagName("frequencia");
+
+			for (int k = 0; k < descricaoTemp.getLength(); k++) {
+				foi = getActualTextForSession(foi, sessao, descricaoTemp,
+						presencaTemp, k);
+
+			}
+		}
+		return foi;
+	}
+
+	private static ArrayList<String> getDateFromWs(ArrayList<Integer> lista,
+			ArrayList<String> foi, String data) {
+		int size = lista.size();
+		for (int i = 0; i < size; i++) {
+			showProgress(i, size);
+
+			ListPresencesCongressmanResponseListPresencesCongressmanResult sessao;
+			try {
+
+				Calendar hoje = new GregorianCalendar();
+
+				SimpleDateFormat df = new SimpleDateFormat();
+				df.applyPattern("dd/MM/yyyy");
+
+				SessoesReunioesSoapStub service = ConnectionWithWsSessions
+						.obterConexao();
+
+				sessao = ConnectionWithWsSessions.receberElementPresenca(
+						service, data, df.format(hoje.getTime()),
+						Integer.toString(lista.get(i)));
+
+				NodeList dias = sessao.get_any()[0].getElementsByTagName("dia");
+
+				foi = getNoListsOfDias(foi, dias, sessao);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return foi;
+	}
+
 }
