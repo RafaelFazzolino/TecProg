119aef808dfa7a8ad18c954c514149787d8c30d8
ThabataG <thabata.helen@gmail.com>
2014-11-08 11:13:58 -0200

    lacing header in util classes

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/util/LimparLista.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/util/LimparLista.java
@@ -1,3 +1,15 @@
+/**
+ * Class: CleanList
+ * 
+ * License: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This class is the based to wipe the deputies that have no data of how many
+ * sessions have attended to. 
+ */
+
 package br.com.MDSGPP.ChamadaParlamentar.util;
 
 import java.util.ArrayList;
--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/util/VerificarData.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/util/VerificarData.java
@@ -1,3 +1,14 @@
+/**
+ * Class: DateVerification.
+ *  
+ * License: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * Class that validates the date.
+ */
+
 package br.com.MDSGPP.ChamadaParlamentar.util;
 
 import java.text.ParseException;
